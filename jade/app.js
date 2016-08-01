f115e759228fd4d78283ffee6309f7c7df8dbfd3
cole sobel <colesobel@coles-MacBook-Air.local>
2016-08-01 10:06:43 -0600

    another

--- jade/app.js
+++ jade/app.js
@@ -27,6 +27,8 @@ app.use('/users', users);
 app.use('/testRoute', users);
 app.use('/vinceTest', users);
 app.use('/cole', users);
+app.use('/another', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
