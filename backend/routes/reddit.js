9b9e6c978df714acef8e639b7450e5c8c19f045e
Michael Tobin <mikel.tobin@gmail.com>
2016-08-02 11:40:33 -0600

    Commit

--- backend/routes/reddit.js
+++ backend/routes/reddit.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/oauth', function(req, res, next) {
-  // console.log(req.body.accessToken);
-  // res.sendStatus(200);
 });
+
+router.get('/', function(req,res,next){
+  res.cookie('cookie', 'yeah');
+})
 
 module.exports = router;
