4f3647ac775e378ad7a6e8efd0d4cd6bbb340b3c
cole sobel <colesobel@coles-MacBook-Air.local>
2016-08-02 11:40:18 -0600

    getting server api calls going

--- backend/routes/google.js
+++ backend/routes/google.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
 router.post('/oauth', function(req, res, next) {
   console.log(req.body.accessToken);
+
   res.sendStatus(200);
 });
 
