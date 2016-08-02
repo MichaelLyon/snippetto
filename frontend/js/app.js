21e9d223235a9dba554b6422652ab041354afadd
Michael Tobin <mikel.tobin@gmail.com>
2016-08-01 20:25:04 -0600

    Reddit api call

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,36 +1,36 @@
 angular.module('myApp', ['ui.router', 'myApp.controllers', 'myApp.services'])
 
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('login')
-  $stateProvider
-    .state('login', {
-      url: '/login',
-      templateUrl: '/views/login.html',
-      controller: 'loginController',
-      controllerAs: 'login'
-    })
-    .state('home', {
-      url: '/home',
-      templateUrl: '/views/home.html',
-      controller: 'homeController',
-      controllerAs: 'home'
-    })
-    .state('news', {
-      url: '/news',
-      templateUrl: '/views/news.html',
-      controller: 'newsController',
-      controllerAs: 'news'
-    })
-    .state('api',{
-      url:'/api',
-      templateUrl:'/views/api.html',
-      controller:'apiController',
-      controllerAs: 'api'
-    })
-    .state('sign_ups', {
-      url: '/signup',
-      templateUrl: '/views/sign_ups.html',
-      controller: 'signupController',
-      controllerAs: 'sign_up'
-    })
+    $urlRouterProvider.otherwise('login')
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            templateUrl: '/views/login.html',
+            controller: 'loginController',
+            controllerAs: 'login'
+        })
+        .state('home', {
+            url: '/home',
+            templateUrl: '/views/home.html',
+            controller: 'homeController',
+            controllerAs: 'home'
+        })
+        .state('news', {
+            url: '/news',
+            templateUrl: '/views/news.html',
+            controller: 'newsController',
+            controllerAs: 'news'
+        })
+        .state('api', {
+            url: '/api',
+            templateUrl: '/views/api.html',
+            controller: 'apiController',
+            controllerAs: 'api'
+        })
+        .state('sign_ups', {
+            url: '/signup',
+            templateUrl: '/views/sign_ups.html',
+            controller: 'signupController',
+            controllerAs: 'sign_up'
+        })
 }])
--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -1,43 +1,39 @@
 angular.module('myApp.controllers', [])
-//raw js here
+    //raw js here
 
 .controller('loginController', ['$http', function($http) {
-  console.log('this is the login controller');
-
+    console.log('this is the login controller');
 }])
 
 .controller('homeController', ['$http', function($http) {
-  console.log('this is the home page');
+    console.log('this is the home page');
 }])
 
 
 .controller('newsController', ['$http', function($http) {
-  var self = this
-  $http.get('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=6acc556fbac84c2aa266476c82b9d4f2').then(function(data) {
-    self.stories = data.data.results
-  })
+    var self = this
+    $http.get('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=6acc556fbac84c2aa266476c82b9d4f2').then(function(data) {
+        self.stories = data.data.results
+    })
 }])
 
 
-.controller('apiController', ['$http', function($http){
-  console.log('api');
-  var clientID = 'e8ZJzbmcLGzGLmwXOkHOBxrqkQ8';
+.controller('apiController', ['$http', function($http) {
+    var getReddit = function() {
+        var requestString = 'https://www.reddit.com/r/webdev/top/.json';
+        request = new XMLHttpRequest();
+        request.onload = proccessResults;
+        request.open("get", requestString, true);
+        request.send();
+    };
 
-  var getReddit = function() {
-  var requestString = "https://www.reddit.com/api/v1/authorize?client_id=" +clientID+"&response_type=code&state=1337&redirect_uri=https://reddit.com&duration=temporary&scope=mysubreddits";
-  request = new XMLHttpRequest();
-  request.onload = proccessResults;
-  request.open("get", requestString, true);
-  request.send();
-};
+    var proccessResults = function() {
+        //console.log(this.responseText);
+        var results = JSON.parse(this.responseText);
+        console.log(results);
+        $http.redditResults = results.data.children[0].data.author;
+    };
 
-  var proccessResults = function() {
-    // console.log(this.responseText);
-    var results = JSON.parse(JSON.stringify(this.responseText));
-    console.log(results);
-    redditResults = results;
-  };
-
-getReddit();
+    getReddit();
 
 }])
