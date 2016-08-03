6bd93bfd6ce26233adf13a04a02e356a6fd7f4ad
Michael Tobin <mikel.tobin@gmail.com>
2016-08-03 15:41:06 -0600

    Working position getter

--- backend/routes/news.js
+++ backend/routes/news.js
@@ -5,12 +5,12 @@ var News = require('../lib/queries')
 
 
 router.post('/setPreferences', function(req, res, next) {
-  var user_id = req.body.user_id
-  var array = Object.keys(req.body)
-  array.splice(array.indexOf('user_id'), 1)
-  var asyncArray = News.createArrayForAsync(req.body.user_id, array)
-  News.series(asyncArray)
-  res.sendStatus(200)
+    var user_id = req.body.user_id
+    var array = Object.keys(req.body)
+    array.splice(array.indexOf('user_id'), 1)
+    var asyncArray = News.createArrayForAsync(req.body.user_id, array)
+    News.series(asyncArray)
+    res.sendStatus(200)
 });
 
 
