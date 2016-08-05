1c46cfafb8475c0fb24dd0d766be990b7e841a40
Michael Tobin <mikel.tobin@gmail.com>
2016-08-04 23:49:30 -0600

    Fixed merge conflict with controllers and queries. Added ricks weather, removed a log. Added coles queries and removed the old.

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -16,8 +16,6 @@ angular.module('myApp.controllers', [])
             };
             $rootScope.currentPosition = pos;
             loadingDone = true;
-            console.log('BOOL = true');
-            console.log($rootScope.currentPosition);
         })
         if (loadingDone === true) {
             clearIntervalAndSuch();
@@ -179,23 +177,8 @@ angular.module('myApp.controllers', [])
 
 
 .controller('weatherController', ['$http', '$rootScope', function($http, $rootScope) {
-    var lat = $rootScope.currentPosition.lat
-    var lng = $rootScope.currentPosition.lng
+    // console.log($rootScope);
     var self = this
-
-    function timeConverter(UNIX_timestamp){
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes(); var sec = a.getSeconds() < 10 ? '0' + a.getSeconds() : a.getSeconds();
-    var sec = a.getSeconds();
-    var mdy = month+ ' ' +date+' '+year
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-    return mdy;
-    }
     $http.post('http://localhost:3000/weather/getWeather', $rootScope.currentPosition).then(function(data) {
         self.weatherData = data.data
         self.city = data.data.name
@@ -218,32 +201,49 @@ angular.module('myApp.controllers', [])
 
           self.day3_icon = obj.data.list[2].weather[0].icon
         })
-
     })
 }])
 
 .controller('trafficController', ['$http', '$rootScope', function($http, $rootScope) {
+  var selfTraffic = this;
+    //User Origin var
     var origin1 = new google.maps.LatLng($rootScope.currentPosition);
+    //Google maps objects for displaying/finding directions/showing traffic
     var directionsService = new google.maps.DirectionsService;
     var directionsDisplay = new google.maps.DirectionsRenderer;
+    var trafficLayer = new google.maps.TrafficLayer();
+    //The map object
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    //What's being sent to backend
     var serverObject = {};
+
     serverObject.origin1 = origin1;
     serverObject.userId = $rootScope.user_id;
+
+    this.trafficSwitch = false;
+
+    //Controls the Traffic view and form input to save addresses
+    if($rootScope.user_id){
+      this.trafficSwitch = true;
+    }
 
     var mapOptions = {
         zoom: 15,
         center: origin1,
         mapTypeId: google.maps.MapTypeId.MAP
     }
+
+    //Setting the map objects
     directionsDisplay.setMap(map);
+    trafficLayer.setMap(map);
 
     $http.post('http://localhost:3000/traffic', serverObject).then(function(data) {
+      selfTraffic.durationToDestination = data.data.durationToWork.text;
         calculateAndDisplayRoute(directionsService, directionsDisplay);
         function calculateAndDisplayRoute(directionsService, directionsDisplay) {
             directionsService.route({
                 origin: origin1,
-                destination: data.data,
+                destination: data.data.destinationCords,
                 travelMode: 'DRIVING'
             }, function(response, status) {
                 if (status === 'OK') {
@@ -256,13 +256,12 @@ angular.module('myApp.controllers', [])
     })
 
     this.workAddGet = function(address) {
-        address.id = $rootScope.user_id;// TODO: REPLACE 1 WITH $rootScope.user_id
+        address.id = $rootScope.user_id;
         $rootScope.workAddress = address;
         $http.post('http://localhost:3000/traffic/setAddress', $rootScope.workAddress).then(function(some) {
 
         })
     }
-
 }])
 
 .controller('todoController', ['$http', '$rootScope', '$state', function($http, $rootScope, $state) {
